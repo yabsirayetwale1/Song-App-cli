267d03edc35dab05743540674d2d63d255a20bfb
yabsirayetwale1 <158767746+yabsirayetwale1@users.noreply.github.com>
2024-02-03 23:19:16 +0300

    Update songs.jsx

--- frontend/src/redux/slice/songs.jsx
+++ frontend/src/redux/slice/songs.jsx
@@ -10,7 +10,6 @@ const songs = createSlice({
     },
     addSongSlice: (state, action) => {
       state.songs.push(action.payload);
-      console.log("action.payload")
     },
     editSongSlice: (state, action) => {
       state.songs = state.songs.map((song) =>

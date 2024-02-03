9f83c3499e5101fa9ddf7900512036c44cedc019
yabsirayetwale1 <158767746+yabsirayetwale1@users.noreply.github.com>
2024-02-03 22:49:06 +0300

    Update index.jsx

--- frontend/src/api/index.jsx
+++ frontend/src/api/index.jsx
@@ -1,13 +1,13 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://song-api.vercel.app'
+axios.defaults.baseURL = 'http://song-app-sigma.vercel.app'
 
-export const getSongsAPI = async () => axios.get('/songs')
+export const getSongsAPI = async () => axios.get('/')
 
-export const getSongByIdAPI = async (id) => axios.get(`/songs/${id}`)
+export const getSongByIdAPI = async (id) => axios.get(`/${id}`)
 
-export const createSongAPI = async (song) => axios.post(`/songs`, song)
+export const createSongAPI = async (song) => axios.post(`/`, song)
 
-export const updateSongAPI = async (song) => axios.patch(`/songs/${song._id}`, song)
+export const updateSongAPI = async (song) => axios.patch(`/${song._id}`, song)
 
-export const deleteSongByIdAPI = async (id) => axios.delete(`/songs/${id}`)
+export const deleteSongByIdAPI = async (id) => axios.delete(`/${id}`)

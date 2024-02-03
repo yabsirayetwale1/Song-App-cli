88fb50e66cfe01359fdd5c148b382d2a08464f16
yabsirayetwale1 <158767746+yabsirayetwale1@users.noreply.github.com>
2024-02-03 23:23:36 +0300

    Update index.jsx

--- frontend/src/api/index.jsx
+++ frontend/src/api/index.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://localhost:5000'
+axios.defaults.baseURL = 'http://song-app-api.vercel.app'
 
 export const getSongsAPI = async () => axios.get('/')
 

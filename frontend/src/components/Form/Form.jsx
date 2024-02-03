d7b136cbd9cd9f30d429e1e8e26c9cc1a97cbb90
yabsirayetwale1 <158767746+yabsirayetwale1@users.noreply.github.com>
2024-02-03 22:53:25 +0300

    Update Form.jsx

--- frontend/src/components/Form/Form.jsx
+++ frontend/src/components/Form/Form.jsx
@@ -33,10 +33,10 @@ const Form = ({ currentId, setCurrentId }) => {
   const dispatch = useDispatch();
   const handleSubmit =async (e) => {
     e.preventDefault();
-    if(!postData.audio){
-      alert ("upload mp3 is required")
-    }
-  else{
+    // if(!postData.audio){
+    //   alert ("upload mp3 is required")
+    // }
+ 
     if( currentId ){
       dispatch({ type: UPDATE_SONG_BY_ID,currentId , song: postData})
       history('/songs')
@@ -45,7 +45,7 @@ const Form = ({ currentId, setCurrentId }) => {
        dispatch({ type: CREATE_SONG,song: postData })
        history('/')
      }
-  }
+  
     clear();
   };
 

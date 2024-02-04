76b232ded459561b252d7790ae3a704a19ac0eb4
yabsirayetwale1 <158767746+yabsirayetwale1@users.noreply.github.com>
2024-02-04 13:39:20 +0300

    Update Form.jsx

--- frontend/src/components/Form/Form.jsx
+++ frontend/src/components/Form/Form.jsx
@@ -1,16 +1,174 @@
+// import React, { useState, useEffect } from "react";
+// import { TextField, Button, Typography, Paper, Select,Container, MenuItem, FormControl, InputLabel } from "@mui/material";
+// import { useDispatch, useSelector } from "react-redux";
+// import { CREATE_SONG, UPDATE_SONG_BY_ID } from "../../redux/actionTypes";
+// import FileBase from "react-file-base64";
+// import styled from "@emotion/styled";
+// import { space, layout} from "styled-system";
+// import {useNavigate} from 'react-router-dom'
+
+// const FormContainer = styled(Paper)(space,{borderRadius: '10px',});
+// const FormTextField = styled(TextField)(space);
+// const FormButton = styled(Button)(space, layout);
+// const FormSelect = styled(Select)(space);
+// const Form = ({ currentId, setCurrentId }) => {
+//   const [postData, setPostData] = useState({
+//     singer: "",
+//     title: "",
+//     genre: "",
+//     imgUrl: "",
+//     audio: ""
+//   });
+//   const history=useNavigate()
+//   const song = useSelector((state) =>
+//     currentId ? state.songs.songs.find((msg) => msg._id === currentId) : null
+//   );
+
+//   useEffect(() => {
+//     if (song) {
+//       setPostData(song);
+//     }
+//   }, [song]);
+
+//   const dispatch = useDispatch();
+//   const handleSubmit =async (e) => {
+//     e.preventDefault();
+//     // if(!postData.audio){
+//     //   alert ("upload mp3 is required")
+//     // }
+ 
+//     if( currentId ){
+//       dispatch({ type: UPDATE_SONG_BY_ID,currentId , song: postData})
+//       history('/songs')
+//      }
+//      else{
+//        dispatch({ type: CREATE_SONG,song: postData })
+//        history('/')
+//      }
+  
+//     clear();
+//   };
+
+//   const clear = () => {
+//     setCurrentId(null);
+//     setPostData({
+//       singer: "",
+//       title: "",
+//       genre: "",
+//       imgUrl: "",
+//       audio: ""
+//     });
+//   };
+
+//   return (
+//    <>
+//    <FormContainer p={3} mb={3}>
+//       <Typography variant="h6">{currentId ? "Edit" : "Add"} Song</Typography>
+//       <form autoComplete="off" noValidate onSubmit={handleSubmit}>
+//         <FormTextField
+//           name="singer"
+//           variant="outlined"
+//           label="Singer"
+//           fullWidth
+//           value={postData.singer}
+//           onChange={(e) => setPostData({ ...postData, singer: e.target.value })}
+//           mb={2}
+//         />
+//         <FormTextField
+//           name="title"
+//           variant="outlined"
+//           label="Title"
+//           fullWidth
+//           value={postData.title}
+//           onChange={(e) => setPostData({ ...postData, title: e.target.value })}
+//           mb={2}
+//         />
+//         <FormControl fullWidth variant="outlined" mb={2}>
+//           <InputLabel>Genre</InputLabel>
+//           <FormSelect
+//             value={postData.genre}
+//             onChange={(e) => setPostData({ ...postData, genre: e.target.value })}
+//             label="Genre"
+//           >
+//             <MenuItem value="Rock">Rock</MenuItem>
+//             <MenuItem value="Pop">Pop</MenuItem>
+//             <MenuItem value="Hip Hop">Hip Hop</MenuItem>
+//             <MenuItem value="Dance">Dance</MenuItem>
+//             <MenuItem value="Electronic">Electronic</MenuItem>
+//           </FormSelect>
+//         </FormControl>
+//         <div>
+//         <div style={{position:'relative',color:'blue',top:'20px',zIndex:'5',width:'90px',height:'20px',background:'white'}}>
+//            upload imgUrl
+//           </div>
+//           <FileBase
+//             type="file"
+//             multiple={false}
+//             onDone={({ base64 }) =>
+//               setPostData({ ...postData, imgUrl: base64 })
+//             }
+//           />
+//         </div>
+        
+//         <div >
+//          <Container  mt={3}>
+//           <div style={{position:'relative',color:'blue',top:'20px',zIndex:'5',width:'90px',height:'20px',background:'white'}}>
+//             upload mp3
+//           </div>
+//          <FileBase
+//             type="file"
+//             multiple={false}
+//             onDone={({ base64 }) =>
+//               setPostData({ ...postData, audio: base64 })
+//             }
+//           />
+//          </Container>
+//         </div>
+//         <FormButton
+//           variant="contained"
+//           color="primary"
+//           size="large"
+//           type="submit"
+//           fullWidth
+//           mt={2}
+//         >
+//           {currentId ? "Update" : "Add"} Song
+//         </FormButton>
+//         <FormButton
+//           variant="contained"
+//           color="secondary"
+//           size="small"
+//           onClick={clear}
+//           fullWidth
+//           mt={2}
+//         >
+//           Clear
+//         </FormButton>
+//       </form>
+//     </FormContainer>
+//     </>
+//   );
+// };
+
+// export default Form;
+
+
 import React, { useState, useEffect } from "react";
-import { TextField, Button, Typography, Paper, Select,Container, MenuItem, FormControl, InputLabel } from "@mui/material";
+import { TextField, Button, Typography, Paper, Select, Container, MenuItem, FormControl, InputLabel } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import { CREATE_SONG, UPDATE_SONG_BY_ID } from "../../redux/actionTypes";
 import FileBase from "react-file-base64";
 import styled from "@emotion/styled";
-import { space, layout} from "styled-system";
-import {useNavigate} from 'react-router-dom'
+import { space, layout } from "styled-system";
+import { useNavigate } from 'react-router-dom'
 
-const FormContainer = styled(Paper)(space,{borderRadius: '10px',});
+const MAX_FILE_SIZE = 10 * 1024 * 1024; // 10MB (adjust as needed)
+
+const FormContainer = styled(Paper)(space, { borderRadius: '10px', });
 const FormTextField = styled(TextField)(space);
 const FormButton = styled(Button)(space, layout);
 const FormSelect = styled(Select)(space);
+
 const Form = ({ currentId, setCurrentId }) => {
   const [postData, setPostData] = useState({
     singer: "",
@@ -19,7 +177,8 @@ const Form = ({ currentId, setCurrentId }) => {
     imgUrl: "",
     audio: ""
   });
-  const history=useNavigate()
+  const [selectedFile, setSelectedFile] = useState(null);
+  const history = useNavigate();
   const song = useSelector((state) =>
     currentId ? state.songs.songs.find((msg) => msg._id === currentId) : null
   );
@@ -31,21 +190,22 @@ const Form = ({ currentId, setCurrentId }) => {
   }, [song]);
 
   const dispatch = useDispatch();
-  const handleSubmit =async (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    // if(!postData.audio){
-    //   alert ("upload mp3 is required")
-    // }
- 
-    if( currentId ){
-      dispatch({ type: UPDATE_SONG_BY_ID,currentId , song: postData})
+    if (selectedFile && selectedFile.size > MAX_FILE_SIZE) {
+      alert("File size exceeds the maximum limit.");
+      return;
+    }
+
+    if (currentId) {
+      dispatch({ type: UPDATE_SONG_BY_ID, currentId, song: postData })
       history('/songs')
-     }
-     else{
-       dispatch({ type: CREATE_SONG,song: postData })
-       history('/')
-     }
-  
+    }
+    else {
+      dispatch({ type: CREATE_SONG, song: postData })
+      history('/')
+    }
+
     clear();
   };
 
@@ -58,94 +218,87 @@ const Form = ({ currentId, setCurrentId }) => {
       imgUrl: "",
       audio: ""
     });
+    setSelectedFile(null);
   };
 
   return (
-   <>
-   <FormContainer p={3} mb={3}>
-      <Typography variant="h6">{currentId ? "Edit" : "Add"} Song</Typography>
-      <form autoComplete="off" noValidate onSubmit={handleSubmit}>
-        <FormTextField
-          name="singer"
-          variant="outlined"
-          label="Singer"
-          fullWidth
-          value={postData.singer}
-          onChange={(e) => setPostData({ ...postData, singer: e.target.value })}
-          mb={2}
-        />
-        <FormTextField
-          name="title"
-          variant="outlined"
-          label="Title"
-          fullWidth
-          value={postData.title}
-          onChange={(e) => setPostData({ ...postData, title: e.target.value })}
-          mb={2}
-        />
-        <FormControl fullWidth variant="outlined" mb={2}>
-          <InputLabel>Genre</InputLabel>
-          <FormSelect
-            value={postData.genre}
-            onChange={(e) => setPostData({ ...postData, genre: e.target.value })}
-            label="Genre"
-          >
-            <MenuItem value="Rock">Rock</MenuItem>
-            <MenuItem value="Pop">Pop</MenuItem>
-            <MenuItem value="Hip Hop">Hip Hop</MenuItem>
-            <MenuItem value="Dance">Dance</MenuItem>
-            <MenuItem value="Electronic">Electronic</MenuItem>
-          </FormSelect>
-        </FormControl>
-        <div>
-        <div style={{position:'relative',color:'blue',top:'20px',zIndex:'5',width:'90px',height:'20px',background:'white'}}>
-           upload imgUrl
-          </div>
-          <FileBase
-            type="file"
-            multiple={false}
-            onDone={({ base64 }) =>
-              setPostData({ ...postData, imgUrl: base64 })
-            }
+    <>
+      <FormContainer p={3} mb={3}>
+        <Typography variant="h6">{currentId ? "Edit" : "Add"} Song</Typography>
+        <form autoComplete="off" noValidate onSubmit={handleSubmit}>
+          <FormTextField
+            name="singer"
+            variant="outlined"
+            label="Singer"
+            fullWidth
+            value={postData.singer}
+            onChange={(e) => setPostData({ ...postData, singer: e.target.value })}
+            mb={2}
           />
-        </div>
-        
-        <div >
-         <Container  mt={3}>
-          <div style={{position:'relative',color:'blue',top:'20px',zIndex:'5',width:'90px',height:'20px',background:'white'}}>
-            upload mp3
-          </div>
-         <FileBase
-            type="file"
-            multiple={false}
-            onDone={({ base64 }) =>
-              setPostData({ ...postData, audio: base64 })
-            }
+          <FormTextField
+            name="title"
+            variant="outlined"
+            label="Title"
+            fullWidth
+            value={postData.title}
+            onChange={(e) => setPostData({ ...postData, title: e.target.value })}
+            mb={2}
           />
-         </Container>
-        </div>
-        <FormButton
-          variant="contained"
-          color="primary"
-          size="large"
-          type="submit"
-          fullWidth
-          mt={2}
-        >
-          {currentId ? "Update" : "Add"} Song
-        </FormButton>
-        <FormButton
-          variant="contained"
-          color="secondary"
-          size="small"
-          onClick={clear}
-          fullWidth
-          mt={2}
-        >
-          Clear
-        </FormButton>
-      </form>
-    </FormContainer>
+          <FormControl fullWidth variant="outlined" mb={2}>
+            <InputLabel>Genre</InputLabel>
+            <FormSelect
+              value={postData.genre}
+              onChange={(e) => setPostData({ ...postData, genre: e.target.value })}
+              label="Genre"
+            >
+              <MenuItem value="Rock">Rock</MenuItem>
+              <MenuItem value="Pop">Pop</MenuItem>
+              <MenuItem value="Hip Hop">Hip Hop</MenuItem>
+              <MenuItem value="Dance">Dance</MenuItem>
+              <MenuItem value="Electronic">Electronic</MenuItem>
+            </FormSelect>
+          </FormControl>
+          <div>
+            <div style={{ position: 'relative', color: 'blue', top: '20px', zIndex: '5', width: '90px', height: '20px', background: 'white' }}>
+              upload imgUrl
+            </div>
+            <FileBase
+              type="file"
+              multiple={false}
+              onDone={({ base64 }) =>
+                setPostData({ ...postData, imgUrl: base64 })
+              }
+            />
+          </div>
+
+          <div>
+            <Container mt={3}>
+              <div style={{ position: 'relative', color: 'blue', top: '20px', zIndex: '5', width: '90px', height: '20px', background: 'white' }}>
+                upload mp3
+              </div>
+              <FileBase
+                type="file"
+                multiple={false}
+                onDone={({ base64, file }) => {
+                  if (file.size > MAX_FILE_SIZE) {
+                    alert("File size exceeds the maximum limit.");
+                    return;
+                  }
+                  setPostData({ ...postData, audio: base64 });
+                  setSelectedFile(file);
+                }}
+              />
+            </Container>
+          </div>
+          <FormButton
+            variant="contained"color="primary" size="large" type="submit" fullWidth>
+            {currentId ? "Save Changes" : "Add Song"}
+          </FormButton>
+          <FormButton variant="contained" color="secondary" size="small" onClick={clear} fullWidth>
+            Clear
+          </FormButton>
+        </form>
+      </FormContainer>
     </>
   );
 };
